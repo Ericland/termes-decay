b3016fe0133a9a1ffa985329b618c0a8ec6f06a9
Jiahe Chen <jc3472@cornell.edu>
2025-01-07 15:32:24 -0500

    show images in the demo

--- demo_build_random_structure.py
+++ demo_build_random_structure.py
@@ -8,6 +8,7 @@ from CRC_Blueprints import generate_random_blueprint
 from CRC_Visualization import visualize_construction 
 from CRC_Policy import generate_CRC_policy
 from CRC_Simulation import simulate_crc
+from matplotlib import pyplot as plt
 
 
 # In[]
@@ -15,8 +16,9 @@ bp = generate_random_blueprint((5, 5), make_plot=True, save_data=True)
 policyInfo_dict = generate_CRC_policy(bp, sol_limit=1, make_plot=True)
 simInfo = simulate_crc(bp, policyInfo_dict[0], robot_num=5, print_info=True, make_plot=True)
 tw, sh, rh = simInfo 
-visualize_construction(tw.start_struct, tw.goal_struct, sh, rh, style='2d', frame_sampling_period=10, video_format='gif')
-visualize_construction(tw.start_struct, tw.goal_struct, sh, rh, style='3d', frame_sampling_period=10, video_format='gif')
+visualize_construction(tw.start_struct, tw.goal_struct, sh, rh, style='2d', frame_sampling_period=100, video_format='gif')
+visualize_construction(tw.start_struct, tw.goal_struct, sh, rh, style='3d', frame_sampling_period=100, video_format='gif')
+plt.show()
 
 
 
